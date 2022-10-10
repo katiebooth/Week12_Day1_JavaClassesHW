92de98dbb639a62e2ef487c27692abd44d4ba089
Katie Booth <katiebooth3@gmail.com>
2022-10-10 15:43:07 +0100

    Finished water bottle

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -4,4 +4,20 @@ public class WaterBottle {
     public WaterBottle(int volume){
         this.volume = volume;
     }
+
+    public int getVolume(){
+        return this.volume;
+    }
+
+    public int drink() {
+        return this.volume -= 10;
+    }
+
+    public int empty() {
+        return this.volume = 0;
+    }
+
+    public int fill() {
+        return this.volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+    @Before
+    public void before() {
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void startingVolumeShouldBe100(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void drinkFunction(){
+        waterBottle.drink();
+        waterBottle.drink();
+        assertEquals(80,waterBottle.getVolume());
+    }
+
+    @Test
+    public void emptyFunction(){
+        waterBottle.empty();
+        assertEquals(0,waterBottle.getVolume());
+    }
+
+    @Test
+    public void fillFunction(){
+        waterBottle.fill();
+        assertEquals(100,waterBottle.getVolume());
+    }
 }
